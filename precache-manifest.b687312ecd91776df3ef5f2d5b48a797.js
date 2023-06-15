96e6cbdec656c6f0d18d20db3741072a27ca8d65
D_Ace <aqibaftab2014@gail.com>
2023-06-15 06:10:44 +0500

    Updates

---
+++ precache-manifest.b687312ecd91776df3ef5f2d5b48a797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c834a82a2da2bdbcab96327abfdcf982",
+    "url": "/bleota/index.html"
+  },
+  {
+    "revision": "8e16a0504660e6650ea8",
+    "url": "/bleota/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/bleota/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bleota/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e16a0504660e6650ea8",
+    "url": "/bleota/static/js/main.e0bb4407.chunk.js"
+  },
+  {
+    "revision": "acbca88d3246a9cd595c",
+    "url": "/bleota/static/js/runtime-main.edf1d00c.js"
+  }
+]);
